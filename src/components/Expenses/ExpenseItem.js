2d7731f00a924a7fbd36ab9812e469aab1adbe01
ziaurrahaman <ziaurrahaman93i@gmail.com>
2021-12-15 15:00:34 +0600

    filter has been added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,16 +3,16 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import React, { useState } from "react";
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => setTitle("Updated!!");
+  // const [title, setTitle] = useState(props.title);
+  // const clickHandler = () => setTitle("Updated!!");
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount} Tk</div>
       </div>
-      <button onClick={clickHandler}>change title</button>
+      {/* <button onClick={clickHandler}>change title</button> */}
     </Card>
   );
 };
